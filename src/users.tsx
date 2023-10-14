7ea035eb4581c2bd70c4e8e676c6d0b79146d0eb
Blake Rand <blake.rand@icloud.com>
2023-10-14 13:05:09 -0400

    Started off backend

---
+++ src/users.tsx
@@ -0,0 +1,66 @@
+const JohnSmith = {
+    "firstName": "John",
+    "lastName": "Smith",
+    "planType": "Unlimited Ultimate",
+    "phonesOnPlan": [
+    {
+        "phoneNumber":  "123-456-7890",
+        "brand": "Apple",
+        "model": "iPhone 15 Pro Max"
+    },
+    {
+        "phoneNumber":  "198-765-4321",
+        "brand": "Samsung",
+        "model": "Galaxy S23"
+    }
+    ]
+}
+
+const JaneDoe = {
+    "firstName": "Jane",
+    "lastName": "Doe",
+    "planType": "Unlimited Plus",
+    "phonesOnPlan": [
+    {
+        "phoneNumber":  "234-567-8901",
+        "brand": "Apple",
+        "model": "iPhone 15"
+    },
+    {
+        "phoneNumber":  "345-678-9012",
+        "brand": "Google",
+        "model": "Pixel 6"
+    },
+    {
+        "phoneNumber":  "456-789-0123",
+        "brand": "Nokia",
+        "model": "3310"
+    }
+    ]
+}
+
+const BobBuilder = {
+    "firstName": "Bob",
+    "lastName": "Builder",
+    "planType": "Unlimited Welcome",
+    "phonesOnPlan": [
+    {
+        "phoneNumber":  "567-890-1234",
+        "brand": "Samsung",
+        "model": "Galaxy S24"
+    },
+    {
+        "phoneNumber":  "678-901-2345",
+        "brand": "Apple",
+        "model": "iPhone 16"
+    },
+    {
+        "phoneNumber":  "789-012-3456",
+        "brand": "OnePlus",
+        "model": "9 Pro"
+    }
+    ]
+}
+
+export const users = [JohnSmith, JaneDoe, BobBuilder];
+
